3a9bf126d6800aba6339a80285842bc23dbce51e
Fruit of Eden <lu.gt@163.com>
2018-04-26 22:44:37 +0800

    seperated in to varibs, added include in CMakeLists.txt

--- varib/sharedsf.cmake
+++ varib/sharedsf.cmake
@@ -46,3 +46,23 @@ set(COMMON_COM_TARG_SRC
   config_host.c
   config_platform.c
 )
+
+foreach(ONE_C_FILE IN LISTS COMMON_COM_SRC)
+  list(APPEND COMMON_SRC ${COMON_COM_DIR}/${ONE_C_FILE})
+endforeach()
+
+foreach(ONE_C_FILE IN LISTS COMMON_COM_TARG_SRC)
+  list(APPEND COMMON_SRC ${COMMON_COM_DIR}/x8664/${ONE_C_FILE})
+endforeach()
+
+foreach(ONE_C_FILE IN LISTS COMMON_COM_CXX_SRC)
+  list(APPEND COMMON_SRC ${COMMON_COM_DIR}/${ONE_C_FILE})
+endforeach()
+
+foreach(ONE_C_FILE IN LISTS COMMON_COM_TARG_CXX_SRC)
+  list(APPEND COMMON_SRC ${COMMON_COM_DIR}/x8664/${ONE_C_FILE})
+endforeach()
+
+foreach(ONE_C_FILE IN LISTS BE_COM_SRC)
+  list(APPEND COMMON_SRC ${BE_COM_DIR}/${ONE_C_FILE})
+endforeach()
