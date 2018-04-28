bfd2f910d8103343920b9b104450a30de181dea5
涂嘉铖 <tujiacheng@jc.lan>
2018-04-29 00:33:18 +0800

    LIBCMPLRS

--- libcmplrs/localsf.cmake
+++ libcmplrs/localsf.cmake
@@ -1,14 +1,14 @@
 #aux_source_directory(${LIBCMPLERS} WGEN_SRC_FOUND)
 
 
-SET(CFILES      
+SET(C_FILES
     array_alloc.c
     make_depend.c
 )
 
 
-foreach(ONE_C_FILE IN CFILES )
-  list(APPEND LIBCMPLRS ${LIB_CMPLR_DIR}/${ONE_C_FILE})
+foreach(ONE_C_FILE IN C_FILES)
+  list(APPEND LIB_CMPLR_SRC ${LIB_CMPLR_DIR}/${ONE_C_FILE})
 endforeach()
 
 
--- libcmplrs/saveenv.cmake
+++ libcmplrs/saveenv.cmake
@@ -1,4 +1,4 @@
-add_executable(wgen ${WGEN_SRC})
-target_compile_options(wgen PUBLIC ${TEMP_C_OPT} ${HOST_COMPILER_OPT} ${LOCAL_COMPLER_OPT} ${HOST_C_OPT} ${LOCAL_C_OPT})
-target_include_directories(wgen PUBLIC ${HOST_INC_DIR} ${LOCAL_INC_DIR})
-target_compile_definitions(wgen PUBLIC ${HOST_DEF} ${LOCAL_DEF})
+add_library(cmplrs ${LIB_CMPLR_SRC})
+target_compile_options(cmplrs PUBLIC ${TEMP_C_OPT} ${HOST_COMPILER_OPT} ${LOCAL_COMPLER_OPT} ${HOST_C_OPT} ${LOCAL_C_OPT})
+target_include_directories(cmplrs PUBLIC ${HOST_INC_DIR} ${LOCAL_INC_DIR})
+target_compile_definitions(cmplrs PUBLIC ${HOST_DEF} ${LOCAL_DEF})
