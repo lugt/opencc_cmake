3ffe56ee474f27a6e357184278ee9708a3314d10
涂嘉铖 <tujiacheng@jc.lan>
2018-04-29 23:49:20 +0800

    LIBCOMPLRS Cmakeable

--- libcmplrs/localsf.cmake
+++ libcmplrs/localsf.cmake
@@ -1,24 +1,17 @@
 #aux_source_directory(${LIBCMPLERS} WGEN_SRC_FOUND)
 
 
-SET(C_FILES
+SET(CFILES
     array_alloc.c
     make_depend.c
 )
 
 
-foreach(ONE_C_FILE IN C_FILES)
+foreach(ONE_C_FILE IN LISTS CFILES)
   list(APPEND LIB_CMPLR_SRC ${LIB_CMPLR_DIR}/${ONE_C_FILE})
 endforeach()
 
 
 
 
-#foreach(ONE_C_FILE IN LISTS FRONT_END_C_SRCS )
-#  list(APPEND WGEN_SRC ${ONE_C_FILE})
-#endforeach()
-
-#foreach(ONE_C_FILE IN LISTS FRONT_END_CXX_SRCS )
-#  list(APPEND WGEN_SRC ${ONE_C_FILE})
-#endforeach()
 
