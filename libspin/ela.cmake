75d1f146c703481a45be5033af8b037ea9ce8d6e
涂嘉铖 <tujiacheng@jc.local>
2018-05-02 16:05:25 +0800

    ela

--- libspin/ela.cmake
+++ libspin/ela.cmake
@@ -4,7 +4,9 @@ set(LOCAL_INC_DIR
   ${OSPREY_SRC_DIR}/include/cmplrs
   ${BUILD_DIR}
   )
-set(LOCAL_DEF )
+set(LOCAL_DEF
+-DFE_GNU_4_2_0
+ )
 set(LOCAL_COMPILER_OPT )
 set(LOCAL_C_OPT )
 set(LOCAL_CPP_OPT )
