0add5273e668f2c72c3b639f8d5134e6fa0afa74
xc5-ubuntu16 <xc5@xc5.io>
2018-06-06 11:05:30 -0700

    Changed Plat recog
    	modified:   global/global.cmake

--- global/global.cmake
+++ global/global.cmake
@@ -18,6 +18,7 @@ set(HOST_INC_DIR
 /include
 )
 
+# Host Platform
 
 set(HOST_C_FLAG -D_LANGUAGE_C)
 set(HOST_CXX_FLAG -D_LANGUAGE_C_PLUS_PLUS -fpermissive)
@@ -28,7 +29,6 @@ set(HOST_COMPILER_OPT -m64 -mtune=generic -march=x86-64
 
 set(HOST_DEF
 	-D_GNU_SOURCE
-        -DBUILD_OS_DARWIN
         -D_SGI_SOURCE
         -D__GNU_BUG_WORKAROUND
         -D_NOTHREADS
@@ -58,4 +58,61 @@ set(HOST_DEF
 
 if(DEBUG)
   set(HOST_COMPILER_OPT ${HOST_COMPILER_OPT} -g -O0)
+endif()
+
+
+# Host(Building) Platform
+
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+MESSAGE(STATUS "###################################")
+
+MESSAGE(STATUS "operation system is ${CMAKE_SYSTEM}")
+
+IF (CMAKE_SYSTEM_NAME MATCHES "Linux")
+  MESSAGE(STATUS "current platform: Linux ")
+  set(BUILD_LINUX 1)
+ELSEIF (CMAKE_SYSTEM_NAME MATCHES "Windows")
+  MESSAGE(STATUS "current platform: Windows")
+    set(BUILD_WINDOWS 1)
+ELSEIF (CMAKE_SYSTEM_NAME MATCHES "FreeBSD")
+  MESSAGE(STATUS "current platform: FreeBSD")
+ELSE ()
+  MESSAGE(STATUS "other platform: ${CMAKE_SYSTEM_NAME}")
+  set(BUILD_MACOS 1)
+ENDIF (CMAKE_SYSTEM_NAME MATCHES "Linux")
+
+MESSAGE(STATUS "###################################")
+
+if(BUILD_MACOS)
+
+  set(HOST_DEF
+    ${HOST_DEF}
+    -DBUILD_OS_DARWIN)
+  
+  set(BUILD_PLATFORM_INC_DIR ${OSPREY_SRC_DIR}/macos/include)
+
+endif()
+
+if(BUILD_LINUX)
+
+  set(HOST_DEF
+    ${HOST_DEF}
+    -DBUILD_OS_LINUX
+    -DBUILD_LINUX)
+  
+  set(BUILD_PLATFORM_INC_DIR ${OSPREY_SRC_DIR}/linux/include)
 endif()
