8c2c2a9e69ce9b508a4ca9dc179b552b2c69200a
till <till@xc5-servers-iMac-Pro.local>
2018-08-11 16:39:49 +0800

    modify saveenv.cmake

--- bgen/cmake/saveenv.cmake
+++ bgen/cmake/saveenv.cmake
@@ -3,11 +3,11 @@ target_compile_options(bgen_util_o PUBLIC ${TEMP_C_OPT} ${HOST_COMPILER_OPT} ${L
 target_include_directories(bgen_util_o PUBLIC ${LOCAL_INC_DIR} ${HOST_INC_DIR})
 target_compile_definitions(bgen_util_o PRIVATE ${HOST_DEF} ${LOCAL_DEF} -DMONGOOSE_BE)
 
-add_library(common_gen_func ${COMMON_WN_FUNCS})
-target_compile_options(common_gen_func PUBLIC ${TEMP_C_OPT} ${HOST_COMPILER_OPT} ${LOCAL_COMPLER_OPT} ${HOST_C_OPT} ${LOCAL_C_OPT})
-target_include_directories(common_gen_func PUBLIC ${LOCAL_INC_DIR} ${HOST_INC_DIR})
-target_compile_definitions(common_gen_func PUBLIC ${HOST_DEF} ${LOCAL_DEF})
-target_link_libraries(common_gen_func comutils spin cmplrs iberty wgen_util_o)
+add_library(bcommon_gen_func ${COMMON_WN_FUNCS})
+target_compile_options(bcommon_gen_func PUBLIC ${TEMP_C_OPT} ${HOST_COMPILER_OPT} ${LOCAL_COMPLER_OPT} ${HOST_C_OPT} ${LOCAL_C_OPT})
+target_include_directories(bcommon_gen_func PUBLIC ${LOCAL_INC_DIR} ${HOST_INC_DIR})
+target_compile_definitions(bcommon_gen_func PUBLIC ${HOST_DEF} ${LOCAL_DEF})
+target_link_libraries(bcommon_gen_func comutils spin cmplrs iberty wgen_util_o)
 
 
 if(DEBUG_CMAKE)
