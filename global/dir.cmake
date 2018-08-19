e3d566a163e146436ddeb689d44ceff47c5712fb
jason.lu <jason-lu.mbp>
2018-08-20 02:26:02 +0800

    Added Dir.cmake Auto Find

--- global/dir.cmake
+++ global/dir.cmake
@@ -1,7 +1,12 @@
 set(DEBUG ON)
-if(IS_DIRECTORY)
+if(IS_DIRECTORY ${CMAKE_SOURCE_DIR}/../../../osprey)
 set(OSPREY_SRC_DIR ${CMAKE_SOURCE_DIR}/../../../osprey)
 endif()
+
+if(IS_DIRECTORY ${CMAKE_SOURCE_DIR}/../mastiff/osprey)
+set(OSPREY_SRC_DIR ${CMAKE_SOURCE_DIR}/../mastiff/osprey)
+endif()
+
 
 
 ######### ###############################################
@@ -12,11 +17,8 @@ endif()
 # set(OSPREY_SRC_DIR <path_to_another_osprey>)
 
 if(IS_DIRECTORY ${OSPREY_SRC_DIR})
-    if(IS_FILE ${OSPREY_SRC_DIR}/../defs.mk)
-        message(STATUS " [Using OSPREY] : [" ${OSPREY_SRC_DIR} "]")
-    else()
-        message(FATAL_ERROR "[ERR_2] : Please double check that the path to the osprey dir is properly set in global/dir.cmake")
-    endif()
+    ####if(IS_FILE ${OSPREY_SRC_DIR}/../defs.mk)
+    message(STATUS " [Using OSPREY] : [" ${OSPREY_SRC_DIR} "]")
 else()
     message(FATAL_ERROR "[ERR_1] Please double check that the path to the osprey dir is properly set in global/dir.cmake")
 endif()
